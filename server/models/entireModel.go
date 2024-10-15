d6e1038db0098e88dfd018550037afe457a13f05
Zack-Ali-Hassan <yares6667@gmail.com>
2024-10-15 21:36:12 +0300

    five commit in 15-oct-2024

--- server/models/entireModel.go
+++ server/models/entireModel.go
@@ -1,1 +1,11 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type CalorieTracker struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Dish        string             `json:"dish"`
+	Fat         float64            `json:"fat"`
+	Ingredients string             `json:"ingredients"`
+	Calories    string             `json:"calories"`
+}
