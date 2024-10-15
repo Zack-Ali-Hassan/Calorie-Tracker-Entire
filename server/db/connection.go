9b47ec230d76cfd029872c78f17948137bc511e1
Zack-Ali-Hassan <yares6667@gmail.com>
2024-10-15 21:09:23 +0300

    third commit in 15-oct-2024

--- server/db/connection.go
+++ server/db/connection.go
@@ -1,6 +1,33 @@
 package db
 
-var Db 
-func ConnectDB() {
+import (
+	"context"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Db *mongo.Database
+
+func ConnectDB() *mongo.Database {
+	err := godotenv.Load(".env")
+	if os.Getenv("ENV") != "production" {
+		if err != nil {
+			log.Fatal("Error loading environment file: ", err)
+		}
+	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOption := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOption)
+	if err != nil {
+		log.Fatal("Error from MongoDB Connection")
+	}
+	err = client.Ping(context.Background(), nil)
+	database := client.Database("golang_entire_db")
+	Db = database
+	return Db
 }
